f506f6fc71cfbe5a27233bb51788558e7ef9b6b3
jamesandroid <jamesandroid@hire-programmers.com>
2022-07-01 21:55:43 +0600

    Adding Bottom Navigation menu

--- app/src/main/java/com/android/raptor/MainActivity.kt
+++ app/src/main/java/com/android/raptor/MainActivity.kt
@@ -1,46 +1,105 @@
 package com.android.raptor
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.android.raptor.R
-import com.android.raptor.ui.theme.RaptorTheme
+import com.android.raptor.ui.theme.Purple700
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            RaptorTheme {
+            HomePageScreen()
 
-
-            }
         }
     }
 }
 
+@Composable
+fun HomePageScreen() {
+    Scaffold(
+        bottomBar = { HomeBottomMenu() }
+    ) {
+
+    }
+}
 
 @Composable
+fun HomeBottomMenu() {
+
+    val context = LocalContext.current
+
+    val selectedItem = remember { mutableStateOf(0) }
+
+    val items = listOf<MenuItem>(
+        MenuItem("Favourite", R.drawable.ic_baseline_favorite_24),
+        MenuItem("Music", R.drawable.ic_baseline_music_note_24),
+        MenuItem("Place", R.drawable.ic_baseline_place_24),
+        MenuItem("News", R.drawable.ic_baseline_fiber_new_24),
+    )
+
+    BottomNavigation(
+        backgroundColor = Purple700
+    ) {
+        items.forEachIndexed { index, menuItem ->
+            BottomNavigationItem(
+                selected = selectedItem.value == index,
+                onClick = {
+                    selectedItem.value = index
+                    Toast.makeText(context, "Clicked on ${menuItem.title}", Toast.LENGTH_SHORT)
+                        .show()
+                },
+                icon = {
+                    Icon(
+                        painter = painterResource(id = menuItem.icon),
+                        contentDescription = null,
+                        tint = Color.Red
+                    )
+                },
+                label = {
+                    Text(
+                        text = menuItem.title,
+                        style = TextStyle(
+                            fontSize = 13.sp,
+                            fontFamily = SplashFont,
+                            color = Color.Red
+                        )
+                    )
+                },
+                alwaysShowLabel = true
+
+            )
+        }
+
+    }
+
+}
+
+data class MenuItem(
+    val title: String,
+    val icon: Int
+)
+
+@Preview
+@Composable
+fun HomePagePreview() {
+    HomePageScreen()
+}
+
+
+/*@Composable
 fun HomePage() {
     Surface(
         modifier = Modifier
@@ -137,12 +196,7 @@ fun HomePage() {
 
 
     }
-}
+}*/
 
-@Preview
-@Composable
-fun HomePagePreview() {
-    HomePage()
-}
 
 
